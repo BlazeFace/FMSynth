469f0413902a3ea41424a6943b2e6360bd4a8d8b
nate <nate.bensing@gmail.com>
2017-04-10 10:48:06 -0400

    Added Runner

--- src/src/Sine.java
+++ src/src/Sine.java
@@ -14,7 +14,7 @@ public class Sine implements Operator {
         waveform = new double[44000];
 
         for(int i = 0; i < waveform.length; i++) {
-            waveform[i] = amplitude * Math.sine((i / waveform.length) * Math.PI);
+            waveform[i] = amplitude * Math.sin((i / waveform.length) * Math.PI);
         }
     }
 
