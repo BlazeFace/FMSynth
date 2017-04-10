8bafde15370a78e921a54c98a69c59fe01fe3ba0
Adam Ido <adam.jendre.ido@gmail.com>
2017-04-10 10:52:27 -0400

    Changed some stuff

--- src/src/Sine.java
+++ src/src/Sine.java
@@ -6,19 +6,19 @@
 public class Sine implements Operator {
     private double freq;
     private double amplitude;
-    private double[] waveform;
+    private double[] samples;
 
     public Sine() {
         freq = 440;
         amplitude = 1;
-        waveform = new double[44000];
+        samples = new double[44000];
 
-        for(int i = 0; i < waveform.length; i++) {
-            waveform[i] = amplitude * Math.sin((i / waveform.length) * Math.PI);
+        for(int i = 0; i < samples.length; i++) {
+            samples[i] = amplitude * Math.sin((i / samples.length) * Math.PI);
         }
     }
 
     public double getFreq() { return freq; }
     public double getAmplitude() { return amplitude; }
-    public double[] getSamples() { return waveform; }
+    public double[] getSamples() { return samples; }
 }
