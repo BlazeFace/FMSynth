3186df96717f2aa1318607a84d8492fca9cbc09d
nate <nate.bensing@gmail.com>
2017-04-10 11:40:48 -0400

    Added Runner

--- src/src/runner.java
+++ src/src/runner.java
@@ -12,10 +12,13 @@ public class runner {
                 operators, // Array of choices
                 operators[1]); // Initial choice
         switch(input){
-            case("Sin"):{Sine a = new Sine();
-            int i = 0;
-            while(i < 20){StdAudio.play(a.getSamples()); i++;}
-            System.out.print("Hello");break;}
+            case("Sin"):{Sine a = new Sine(); int i = 0;
+                while(i < 5){StdAudio.play(a.getSamples()); i++;}break;}
+
+            case("Sawtooth"):{Sawtooth a = new Sawtooth();int i = 0;
+                while(i < 5){StdAudio.play(a.getSamples()); i++;}break;}
         }
     }
 }
+
+
